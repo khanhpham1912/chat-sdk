9cc60ef5153175d715c1606bf97e4b4437db0a85
Khanh Pham <khanh.pham@wareflex.io>
2024-12-06 16:33:46 +0700

    init build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
